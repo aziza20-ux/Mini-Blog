26f8d336506efdaa2d2b8713f581e04349765328
Aziza Solace Afadhali <s.afadhali@alustudent.com>
2026-09-20 11:25:47 +0200

    feat: implement initial Post data and update components to utilize it

---
+++ mini-blog/src/App.tsx
@@ -0,0 +1,15 @@
+import {HeaderComponent} from './components/HeaderComponent';
+import {PostListComponent} from './components/PostListComponent';
+import './App.css'
+
+import { posts } from './data/posts';
+
+
+export const App=()=>{
+  return(
+    <>
+    <HeaderComponent/>
+    <PostListComponent posts={posts}/>
+    </>
+  )
+}
--- mini-blog/src/components/PostListComponent.tsx
+++ mini-blog/src/components/PostListComponent.tsx
@@ -1,31 +1,10 @@
 import type { PostType } from '../types/post';
-import type { FC } from 'react';
+
 
 interface Postprops{
     posts:PostType[]
 }
 
-
-const posts: PostType[] = [
-  {
-    author: "Aziza",
-    title: "Getting Started with React",
-    desc: "A beginner-friendly introduction to building user interfaces with React.",
-    date: new Date("2026-09-15"),
-  },
-  {
-    author: "John",
-    title: "Understanding TypeScript",
-    desc: "Learn how TypeScript adds type safety and better developer experience to JavaScript.",
-    date: new Date("2026-09-17"),
-  },
-  {
-    author: "Alice",
-    title: "Building Components",
-    desc: "Explore how reusable components can help you build maintainable React applications.",
-    date: new Date("2026-09-20"),
-  },
-];
 
 export const PostListComponent= ({posts}:Postprops)=>{
     return(
